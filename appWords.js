7d5484fae369990555962e1915511b5e35a98fc6
Alex Mur <lehamuraschov2014@yandex.ru>
2021-12-06 15:19:24 +0200

    fix: fix linting format errors

--- appWords.js
+++ appWords.js
@@ -1,69 +1,65 @@
-let words = {
-    referense: 'ссылка, справка',
-    sample: 'образец, пример, проба',
-    separate: 'разделять, разъединять',
-    supply: 'поставка, снабжение, питание',
-    hoisting: 'всплытие, поднятие',
-    update: 'обновление, модификация, новая версия',
-    resolve: 'разрешить, решить',
-    reject: 'отклонять, отвергать',
-    consumer:'потребитель, клиент',
-    invoke: 'вызывать, применять',
-    handler: 'обработчик',
-    fluent: 'беглый, текучий, гладкий',
-    template: 'шаблон',
-    contain: 'содержать, включать в себя',
-    bottom: 'дно, нижняя часть',
-    conventional: 'стандартный, общепринятый, обычный',
+const words = {
+  referense: 'ссылка, справка',
+  sample: 'образец, пример, проба',
+  separate: 'разделять, разъединять',
+  supply: 'поставка, снабжение, питание',
+  hoisting: 'всплытие, поднятие',
+  update: 'обновление, модификация, новая версия',
+  resolve: 'разрешить, решить',
+  reject: 'отклонять, отвергать',
+  consumer: 'потребитель, клиент',
+  invoke: 'вызывать, применять',
+  handler: 'обработчик',
+  fluent: 'беглый, текучий, гладкий',
+  template: 'шаблон',
+  contain: 'содержать, включать в себя',
+  bottom: 'дно, нижняя часть',
+  conventional: 'стандартный, общепринятый, обычный',
+};
+
+function arrChoiceElRnd(obj) {
+// получаем массив из 5 случайных и уникальных слов
+
+  const arrWordsKeys = Object.keys(obj);
+  let arr = [];
+  const setWords = new Set();
+  for (let i = 0; i < arrWordsKeys.length; i++) {
+    arr[i] = arrWordsKeys[(Math.floor(Math.random() * (arrWordsKeys.length)))];
+    setWords.add(arr[i]);
+    arr = Array.from(setWords);
+    if (arr.length >= 5) break;
+  }
+  return arr;
 }
 
+const arrWordsRnd = arrChoiceElRnd(words);
 
-function arrChoiceElRnd(obj){
-//получаем массив из 5 случайных и уникальных слов
+// получаем массив слов с переводом
+const wordsWithTransfer = arrWordsRnd.map((item) => `${item} - ${words[item]} `);
 
-    let arrWordsKeys = Object.keys(obj);
-    let arr = [];
-    let setWords = new Set();
-        for (let i = 0; i<arrWordsKeys.length; i++){
-            arr[i] = arrWordsKeys[(Math.floor(Math.random() *(arrWordsKeys.length)))];
-                setWords.add(arr[i]);
-                arr =Array.from(setWords);
-                    if(arr.length>=5)
-                        break;
-        }
-    return arr;
-}
-
-let arrWordsRnd = arrChoiceElRnd(words);
-
-//получаем массив слов с переводом
-let wordsWithTransfer = arrWordsRnd.map(item =>item + ' - ' + words[item] + ' ');
-
-//делаем список из глаголов с переводом
+// делаем список из глаголов с переводом
 const div = document.createElement('div');
 div.classList.add('wrapper');
 
-const body = document.body;
+const { body } = document;
 body.appendChild(div);
 body.style.background = 'rgb(175, 201, 204';
 
 const ulWodsWithTrans = document.createElement('ul');
 div.appendChild(ulWodsWithTrans);
 
-function createUlFromArr (arr){
-
-    arr.forEach(item => {
-        let liElement = document.createElement('li');
-        liElement.classList.add('liWord');
-        liElement.textContent = item;
-        ulWodsWithTrans.appendChild(liElement);
-    });
+function createUlFromArr(arr) {
+  arr.forEach((item) => {
+    const liElement = document.createElement('li');
+    liElement.classList.add('liWord');
+    liElement.textContent = item;
+    ulWodsWithTrans.appendChild(liElement);
+  });
 }
 
 createUlFromArr(wordsWithTransfer);
 
-
-let divResAll = document.createElement('div');
+const divResAll = document.createElement('div');
 body.appendChild(divResAll);
 divResAll.classList.add('allDivTranslate');
 
@@ -88,94 +84,59 @@ btnStartAgain.innerHTML = 'ЕЩЕ РАЗ';
 btnStart.classList = 'btnStart';
 btnStartAgain.addEventListener('click', () => location.reload());
 
-let divContainButton = document.createElement('div');
+const divContainButton = document.createElement('div');
 divContainButton.classList = 'divContainButton';
 divContainButton.appendChild(btnShowResult);
 divContainButton.appendChild(btnStartAgain1);
 
+// проверяем перевод и получаем объект с массивами результатами перевода
 
-//проверяем перевод и получаем объект с массивами результатами перевода
+const wordsResultTranslate = {};
 
-let wordsResultTranslate = {};
+function startTranslate(arr) {
+  const arrResultTranslate = [];
+  const arrWordsInput = [];
+  div.remove();
+  btnStart.remove();
 
-function startTranslate (arr){
-
-    let arrResultTranslate = [];
-    let arrWordsInput = [];
-    div.remove();
-    btnStart.remove();
-
-    for(let i=0; i<arr.length; i++){
-        let a = prompt('Переведите на английский: ' + words[arr[i]].toUpperCase(), 'перевод');
-        if(arr[i] === a){
-            arrResultTranslate[i]= true;
-            arrWordsInput[i] = a;
-            alert( 'Правильный перевод' );
-            }else if (a===null){
-                break;}
-            else{
-                arrResultTranslate[i]= false;
-                arrWordsInput[i] = a;
-                alert( `Вы ошиблись, правильный перевод: ${arr[i].toUpperCase()}`  );
-            }
+  for (let i = 0; i < arr.length; i++) {
+    const a = prompt(`Переведите на английский: ${words[arr[i]].toUpperCase()}`, 'перевод');
+    if (arr[i] === a) {
+      arrResultTranslate[i] = true;
+      arrWordsInput[i] = a;
+      alert('Правильный перевод');
+    } else if (a === null) {
+      break;
+    } else {
+      arrResultTranslate[i] = false;
+      arrWordsInput[i] = a;
+      alert(`Вы ошиблись, правильный перевод: ${arr[i].toUpperCase()}`);
     }
-    wordsResultTranslate.key1 = arrResultTranslate;
-    wordsResultTranslate.key2 = arrWordsInput;
-    body.appendChild(divContainButton);
-    //body.appendChild(divContainButton);
+  }
+  wordsResultTranslate.key1 = arrResultTranslate;
+  wordsResultTranslate.key2 = arrWordsInput;
+  body.appendChild(divContainButton);
+  // body.appendChild(divContainButton);
 }
 
+function showResultTranslate() {
+  btnShowResult.remove();
+  btnStartAgain1.remove();
+  body.appendChild(btnStartAgain);
 
-function showResultTranslate(){
+  for (let i = 0; i < wordsResultTranslate.key1.length; i++) {
+    const divRes = document.createElement('div');
+    divRes.classList = 'translate';
 
-    btnShowResult.remove();
-    btnStartAgain1.remove();
-    body.appendChild(btnStartAgain);
-
-    for (let i=0; i<wordsResultTranslate.key1.length; i++){
-
-        let divRes = document.createElement('div');
-        divRes.classList = 'translate';
-
-    if(wordsResultTranslate.key1[i] === false){
-
-        divRes.innerHTML = ` Вы ответили - ${wordsResultTranslate.key2[i]} <br>
+    if (wordsResultTranslate.key1[i] === false) {
+      divRes.innerHTML = ` Вы ответили - ${wordsResultTranslate.key2[i]} <br>
                              правильно - ${arrWordsRnd[i]}`;
-        divRes.style.border = '2px outset red';
-    } else{ divRes.textContent = ' Верно';
-            divRes.style.border = '2px outset green';
-          }
+      divRes.style.border = '2px outset red';
+    } else {
+      divRes.textContent = ' Верно';
+      divRes.style.border = '2px outset green';
+    }
 
     divResAll.appendChild(divRes);
-    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
